8b43a7f6f19023737b5b502ba4a39ab77d35744b
Slava Gerts <slava.gerts@invitae.com>
2024-01-11 22:35:25 +0100

    updates example with epsilon

--- content/primitives.js
+++ content/primitives.js
@@ -261,7 +261,7 @@ export const content = [
       '  return Math.abs(a - b) < Number.EPSILON',
       '}',
       ' ',
-      'numbersCloseEnoughToEqual(0.1, 0.2)    // true'
+      'numbersCloseEnoughToEqual(0.1 + 0.2, 0.3)    // true'
     ],
     references: [
       {
